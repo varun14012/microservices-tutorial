8a6bdd9766c47d32ff3ad4eac8f5fad54c4099ae
varun14012 <varun14012@gmail.com>
2020-01-02 15:29:37 +0530

    skip actuator security

--- api-gateway-server/src/main/java/io/varun/apigatewayserver/config/auth/WebSecurityConfig.java
+++ api-gateway-server/src/main/java/io/varun/apigatewayserver/config/auth/WebSecurityConfig.java
@@ -43,8 +43,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/authenticate", "http://localhost:8761/")
-				.permitAll().anyRequest().authenticated().and().exceptionHandling().and().sessionManagement()
+		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/authenticate", "/actuator/**").permitAll()
+				.anyRequest().authenticated().and().exceptionHandling().and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
