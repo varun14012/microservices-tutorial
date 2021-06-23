2f8dbbdb25e4ff31d65db4244274fd4a6bb152e3
new pc <new pc@10.198.12.35>
2021-06-23 15:56:15 +0530

    Added exempted url in security conf

--- auth-server/src/main/java/io/varun/authserver/config/auth/WebSecurityConfig.java
+++ auth-server/src/main/java/io/varun/authserver/config/auth/WebSecurityConfig.java
@@ -42,8 +42,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 
-		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/authenticate", "http://localhost:8761/")
-				.permitAll().anyRequest().authenticated().and().exceptionHandling().and().sessionManagement()
+		httpSecurity.csrf().disable().authorizeRequests()
+				.antMatchers("/authenticate", "/authenticate-custom-token", "http://localhost:8761/").permitAll()
+				.anyRequest().authenticated().and().exceptionHandling().and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
