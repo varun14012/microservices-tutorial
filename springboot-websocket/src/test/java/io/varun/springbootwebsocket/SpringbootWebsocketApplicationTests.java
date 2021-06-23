54408055023a3efe7f882c3a45902de8d2053059
new pc <new pc@10.198.12.35>
2021-06-23 15:30:25 +0530

    added ocnf file websocket proj

---
+++ springboot-websocket/src/test/java/io/varun/springbootwebsocket/SpringbootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package io.varun.springbootwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
